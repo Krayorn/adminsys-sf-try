f00b27366396249cebf027eef00f21f6f54c1665
Nathael Arki <nathael.arki@supinternet.fr>
2017-12-06 14:49:22 +0100

    more test with capistrano

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -17,11 +17,15 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
+role :app, %w{deploy@46.101.245.4:22}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
 
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/git/sf-try"
+set :log_level, ENV['LOG_LEVEL']
+set :format, :pretty
 
 # Configuration
 # =============
@@ -49,13 +53,13 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+# server "46.101.245.4",
+#   user: "deploy",
+#   roles: %w{app},
 #   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     user: "deploy", # overrides user setting above
+#     keys: %w(/home/deploy/.ssh/id_rsa),
 #     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
+#     auth_methods: %w(password)
+#     password: ""
 #   }
